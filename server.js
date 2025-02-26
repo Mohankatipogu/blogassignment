f447a043bb34fccf7cae9e5127782f97894e4faa
Mohankatipogu <mohanmadiga225@gmail.com>
2025-02-26 14:35:06 +0530

    Fix dependencies

--- server.js
+++ server.js
@@ -169,7 +169,7 @@ app.get("/", async (req, res) => {
 });
 
 // ✅ Start Server
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
 }).on("error", (err) => {
   console.error("❌ Server Error:", err);
